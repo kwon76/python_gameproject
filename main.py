e14adf06bda943a12a88eb26a1fc98d33e612220
권준혁 <robot0004@gmail.com>
2022-07-17 14:21:26 +0000

    project_commit 3

--- main.py
+++ main.py
@@ -56,6 +56,22 @@ character_height = character_size[1]
 character_x_pos = (screen_width - character_width) / 2
 character_y_pos = screen_height - character_height - stage_height
 
+#캐릭터 이동 방향
+character_to_x = 0
+
+#캐릭터 이동 속도
+character_speed = 5
+
+#무기 만들기
+weapon = pygame.image.load(os.path.join(image_path, "weapon_basic.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+#무기는 한 번에 여러 발 발사 가능하다고 가정
+weapons = []
+
+#무기 이동 속도
+weapon_speed = 10
 
 running = True  
 while running:
@@ -66,14 +82,45 @@ while running:
     if event.type == pygame.QUIT:  
         running = False  
 
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        character_to_x -= character_speed
+      elif event.key == pygame.K_RIGHT:
+        character_to_x += character_speed
+      elif event.key == pygame.K_SPACE: #무기 발사
+        weapon_x_pos = character_x_pos + (character_width - weapon_width) / 2
+        weapon_y_pos = character_y_pos
+        weapons.append([weapon_x_pos, weapon_y_pos])
+
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        character_to_x = 0
+
   #3. 게임 캐릭터 위치 정의
+  character_x_pos += character_to_x
+
+  if character_x_pos < 0:
+    character_x_pos = 0
+  elif character_x_pos > screen_width - character_width:
+    character_x_pos = screen_width - character_width
+
+  #무기 위치 조정
+  weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
+  #x는 그대로 두고, y좌표는 원래값에서 weapon_speed만큼 줄어들어야 함 -> 올라가는 동작
+
+  #천장에 닿은 무기 없애기
+  weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
+  #weapon의 y좌표값이 0보다 클 경우(천장에 닿지 않은 무기들)의 무기들만 인덱싱하기
 
   #4. 충돌 체크
   
-  #5. 화면에 출력
+  #5. 화면에 출력(위에서부터 아래 순서로 그려짐)
   screen.blit(background, (0, 0))
+  for weapon_x_pos, weapon_y_pos in weapons:
+    screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
   screen.blit(stage, (0, screen_height - stage_height))
-  screen.blit(character, (character_x_pos, character_y_pos))
+  screen.blit(character, (character_x_pos, character_y_pos))  
+  
   
   pygame.display.update()  
       
